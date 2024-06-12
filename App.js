34f218973fca345051bab4a713bb057bbe82db2b
Trân Hà Bảo <22521505@ms.uit.edu.vn>
2024-06-12 16:43:00 +0700

    start screen

--- App.js
+++ App.js
@@ -4,10 +4,20 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CameraScreen from './screens/CameraScreen';
 import HistoryScreen from './screens/HistoryScreen';
+import StartScreen from './screens/StartScreen';
+
+import {useFonts} from 'expo-font'
 
 const Stack = createNativeStackNavigator();
 
+
 export default function App() {
+  const [fontsLoaded]=useFonts({
+    OpenSans: require('./assets/fonts/OpenSans-Regular.ttf'),
+    OpenSansBold: require('./assets/fonts/OpenSans-Bold.ttf'),
+    OtomanopeeOne: require('./assets/fonts/OtomanopeeOne-Regular.ttf'),
+  })
+
   return (
     <NavigationContainer>
       <StatusBar />
@@ -15,10 +25,11 @@ export default function App() {
         screenOptions={{
           headerShown: false,
         }}
-        initialRouteName='Camera'
+        initialRouteName='Start'
 
       
       >
+        <Stack.Screen name ="Start" component={StartScreen}/>
         <Stack.Screen name="Camera" component={CameraScreen} />
         <Stack.Screen name="History" component={HistoryScreen} />
       </Stack.Navigator>
---
+++ screens/StartScreen.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Text, View, StyleSheet, ImageBackground, SafeAreaView, StatusBar, TouchableOpacity } from 'react-native';
+import { SvgXml } from 'react-native-svg';
+import leaficon from '../assets/leaf-icon';
+const sourcebr = require('../assets/image.png');
+
+const StartScreen = () => {
+  console.log(leaficon); 
+
+  if (!leaficon) {
+    console.error('SVG XML is undefined'); 
+  }
+
+  return ( 
+    <ImageBackground style={{height:'100%',width:'100%'}} source={sourcebr}>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={{flex:1}}>
+        <View style={{height:'15%',width:'30%', marginTop:170, marginLeft:80}}>
+          <SvgXml style={{marginTop:105, marginLeft:67, width: 59,height: 64,}} xml={leaficon}/>
+        </View>
+        <Text style={styles.our}>our</Text>
+        <Text style={styles.zone}>Z ne</Text>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity style={styles.btnsignup}>
+            <Text style={styles.btnText}>Sign up</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity style={styles.btnsignin}>
+            <Text style={styles.btnText}>Did you have account? Sign in</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  our: {
+    fontSize: 100, 
+    letterSpacing: -0.02,
+    color: '#FFFFFF',
+    fontFamily:'OtomanopeeOne',
+    marginTop:-175, 
+    marginLeft:10
+  },
+  zone: {
+    fontSize: 100, 
+    letterSpacing: -0.02,
+    color: '#738F81',
+    fontFamily:'OtomanopeeOne',
+    marginTop:-38, 
+    marginLeft:80
+  },
+  btnContainer: {
+    alignItems: 'center',
+    marginTop: 20
+  },
+  btnsignup: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#738F81',
+    width: 150,
+    height: 55,
+    borderRadius: 35,
+    marginTop:195,
+  },
+  btnText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontFamily:'OpenSansBold',
+  },
+  btnsignin:
+  {
+
+  }
+});
+
+export default StartScreen;
