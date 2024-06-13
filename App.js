ba886e971666f6c3ba2d7751f92d7cf002771c5e
Trân Hà Bảo <22521505@ms.uit.edu.vn>
2024-06-13 14:11:00 +0700

    sign in email ui

--- App.js
+++ App.js
@@ -4,11 +4,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CameraScreen from './screens/CameraScreen';
 import HistoryScreen from './screens/HistoryScreen';
-
 import StartScreen from './screens/StartScreen';
-import { Asset } from 'expo-asset'
+
+import SigninEmail from './screens/SignIn/SigninemailScreen';
+
+
+// import { Asset } from 'expo-asset'
 import {useFonts} from 'expo-font';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+import * as SplashScreen from 'expo-splash-screen';
+import {useEffect} from 'react';
+
 
 
 const Stack = createNativeStackNavigator();
@@ -20,6 +27,21 @@ export default function App() {
     OpenSansBold: require('./assets/fonts/OpenSans-Bold.ttf'),
     OtomanopeeOne: require('./assets/fonts/OtomanopeeOne-Regular.ttf'),
   })
+  useEffect(()=>{
+    async function prepare(){
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, [])
+
+  if(!fontsLoaded)
+    {
+      return undefined;
+    }
+    else 
+    {
+      SplashScreen.hideAsync();
+    }
 
   return (
     <NavigationContainer>
@@ -31,12 +53,14 @@ export default function App() {
           screenOptions={{
             headerShown: false,
           }}
-          initialRouteName='Start'
+          initialRouteName='SigninEmail'
         
         >
+                 <Stack.Screen name ="SigninEmail" component={SigninEmail}/>
           <Stack.Screen name ="Start" component={StartScreen}/>
           <Stack.Screen name="Camera" component={CameraScreen} />
           <Stack.Screen name="History" component={HistoryScreen} />
+          
         </Stack.Navigator>
       </GestureHandlerRootView>
     </NavigationContainer>
