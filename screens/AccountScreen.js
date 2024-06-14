b5cc24bb5f28d52b21f22afb68ae1c827f88194c
nguyenkhoaquan161004 <khoaquan312@gmail.com>
2024-06-14 18:53:22 +0700

    được 1 khúc nữa rồi nè

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -9,7 +9,13 @@ import iconBack from "../assets/back-icon";
 import iconChangeAvatar from "../assets/change-avatar-icon";
 import iconTheme from "../assets/theme-icon";
 import iconChangeTheme from "../assets/change-theme-icon";
-import iconArrowNext from "../assets/arrow-next-icon"
+import iconArrowNext from "../assets/arrow-next-icon";
+import iconGeneral from "../assets/general-icon";
+import iconChangePhoneNumber from "../assets/change-phone-num-icon";
+import iconChangeEmail from "../assets/change-email-icon";
+import iconChangePassword from "../assets/change-password-icon";
+import iconToS from "../assets/tos-icon";
+import iconPP from "../assets/pp-icon";
 
 
 const AccountScreen = ({ navigation }) => {
@@ -25,12 +31,48 @@ const AccountScreen = ({ navigation }) => {
                 </TouchableOpacity>
             </View>
 
-            <ScrollView style={styles.container} >
+            <ScrollView style={styles.containerScrollView} >
                 <View style={styles.bgAvatar} >
                     <Image style={styles.imgAvatar} source={require('../assets/avatar-picture.jpg')} />
                 </View>
                 <TouchableOpacity style={styles.btnChangeAvatar}>
                     <SvgXml style={styles.iconChangeAvatar} xml={iconChangeAvatar}></SvgXml>
+                </TouchableOpacity>
+
+                <View style={styles.frmName}>
+                    <Text style={styles.textName}>Toi da khoc vi dl.!</Text>
+                </View>
+
+                <TouchableOpacity style={styles.btnChangeName}>
+                    <Text style={{
+                        fontSize: 20, fontWeight: "700", letterSpacing: 0.7, color: "white",
+                        alignContent: "center", marginHorizontal: "auto", marginVertical: "auto"
+                    }}>Edit Info</Text>
+                </TouchableOpacity>
+
+                <View style={styles.frmTopic}>
+                    <SvgXml style={styles.icon} xml={iconTheme}></SvgXml>
+                    <Text style={styles.textTopic}>Theme</Text>
+                </View>
+
+                <TouchableOpacity style={styles.btnNormal}>
+                    <View style={styles.startSection}>
+                        <SvgXml style={styles.icon} xml={iconChangeTheme}></SvgXml>
+                        <Text style={styles.textInBtn}>Change Theme</Text>
+                    </View>
+                    <SvgXml style={styles.icon} xml={iconArrowNext}></SvgXml>
+                </TouchableOpacity>
+
+                <View style={styles.frmTopic}>
+                    <SvgXml style={styles.icon} xml={iconGeneral}></SvgXml>
+                </View>
+
+                <TouchableOpacity style={styles.btnStart}>
+                    <View style={styles.startSection}>
+                        <SvgXml style={styles.icon} xml={iconChangePhoneNumber}></SvgXml>
+                        <Text style={styles.textInBtn}>Change Phone Numbers</Text>
+                    </View>
+                    <SvgXml style={styles.icon} xml={iconArrowNext}></SvgXml>
                 </TouchableOpacity>
             </ScrollView>
 
@@ -44,9 +86,18 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'column',
         flex: 1,
-        paddingHorizontal: 20,
-        backgroundColor: '#F8FFF8'
+        paddingHorizontal: 10,
+        backgroundColor: '#F8FFF8',
+        justifyContent: 'center'
         // position: 'absolute'
+    },
+
+    containerScrollView: {
+        display: 'flex',
+        flexDirection: 'column',
+        flex: 1,
+        paddingHorizontal: 0,
+        backgroundColor: '#F8FFF8'
     },
 
     btnBack: {
@@ -63,6 +114,10 @@ const styles = StyleSheet.create({
         margin: "auto"
     },
 
+    icon: {
+        marginRight: 10
+    },
+
     bgAvatar: {
         width: 144,
         height: 144,
@@ -72,6 +127,7 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: '#AAC2B3',
+        marginBottom: "auto",
     },
 
     imgAvatar: {
@@ -91,13 +147,119 @@ const styles = StyleSheet.create({
         borderWidth: 4,
         borderRadius: 50,
         borderColor: '#FFFFFF',
-        marginHorizontal: 165,
+        marginHorizontal: 190,
         marginVertical: 205
     },
 
     iconChangeAvatar: {
         margin: "auto"
-    }
+    },
+
+    frmName: {
+        marginHorizontal: 'auto',
+        paddingVertical: 3,
+        justifyContent: 'center',
+        marginTop: 10,
+        marginBottom: 10
+    },
+
+    textName: {
+        alignContent: 'center',
+        fontSize: 20,
+        fontWeight: "700",
+        letterSpacing: 0.7,
+        color: '#6B9080',
+        fontSize: 30,
+    },
+
+    btnChangeName: {
+        width: 120,
+        height: 35,
+        marginHorizontal: "auto",
+        backgroundColor: '#738F81',
+        borderRadius: 50,
+        marginBottom: 20
+    },
+
+    frmTopic: {
+        flexDirection: "row",
+        alignSelf: "flex-start",
+        paddingHorizontal: 10,
+        marginBottom: 10
+    },
+
+    textTopic: {
+        fontSize: 20,
+        fontWeight: "800",
+        letterSpacing: 0.4,
+        color: "#6B9080",
+    },
+
+    btnNormal: {
+        width: '95%',
+        height: 50,
+        backgroundColor: '#AAC2B3',
+        borderRadius: 13,
+        marginBottom: 20,
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginHorizontal: "auto",
+        padding: 10,
+    },
+
+    startSection: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    textInBtn: {
+        fontSize: 20,
+        fontWeight: "600",
+        letterSpacing: 0.4,
+        color: "#FFFFFF",
+        marginVertical: "auto",
+    },
+
+    btnStart: {
+        width: '95%',
+        height: 50,
+        backgroundColor: '#AAC2B3',
+        borderTopStartRadius: 13,
+        borderTopEndRadius: 13,
+        marginBottom: 10,
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginHorizontal: "auto",
+        padding: 10
+    },
+
+    btnMid: {
+        width: '95%',
+        height: 50,
+        backgroundColor: '#AAC2B3',
+        marginBottom: 10,
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginHorizontal: "auto",
+        padding: 10
+    },
+
+    btnEnd: {
+        width: '95%',
+        height: 50,
+        backgroundColor: '#AAC2B3',
+        borderBottomEndRadius: 13,
+        borderBottomStartRadius: 13,
+        marginBottom: 10,
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginHorizontal: "auto",
+        padding: 10
+    },
 });
 
 export default AccountScreen;
