2236aaa642be49a3c0c8e29b0f1c84e8fd90d4f6
nguyenkhoaquan161004 <khoaquan312@gmail.com>
2024-06-15 16:33:01 +0700

    Change email screen

--- screens/changeInfo/ChangeEmailScreen.js
+++ screens/changeInfo/ChangeEmailScreen.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, Image, Touchable } from 'react-native';
 import Animated, { SlideInDown, SlideInUp, SlideOutDown, SlideOutUp } from "react-native-reanimated";
+import { SvgXml } from 'react-native-svg';
+//icon
+import iconBack from "../../assets/back-icon";
+import { TextInput } from 'react-native-gesture-handler';
 
 const ChangeEmailScreen = ({ navigation }) => {
+    const [text, setText] = useState('');
     return (
         <Animated.View
             style={styles.container}
             entering={SlideInDown}
             exiting={SlideOutDown}
         >
-            <View>
-                <Text>Change Email</Text>
+            <TouchableOpacity style={styles.btnBack} onPress={() => navigation.navigate('Account')}>
+                <SvgXml style={styles.icon} xml={iconBack}></SvgXml>
+            </TouchableOpacity>
+
+            <View style={styles.frmTopic}>
+                <Text style={styles.textTopic}
+
+                >Enter Your Email</Text>
             </View>
+
+            <TextInput style={styles.frmEdit}
+                placeholder="Enter your Email"
+                value={text}
+                onChange></TextInput>
+
+
+            <TouchableOpacity style={styles.btnSave}>
+                <Text style={styles.textInBtn}>Save</Text>
+            </TouchableOpacity>
+
+
         </Animated.View>
 
     );
@@ -20,12 +43,73 @@ const ChangeEmailScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         display: 'flex',
-        flexDirection: 'column',
         flex: 1,
         paddingHorizontal: 10,
         backgroundColor: '#F8FFF8',
-        justifyContent: 'center'
     },
+
+    btnBack: {
+        width: 50,
+        height: 50,
+        position: 'absolute',
+        top: 103,
+        verticalAlign: 10,
+        backgroundColor: 'transparent'
+    },
+
+    icon: {
+        margin: "auto"
+    },
+
+    frmTopic: {
+        alignContent: "center",
+        marginHorizontal: "auto",
+        marginVertical: 300,
+        marginBottom: 25
+    },
+
+    textTopic: {
+        fontWeight: "800",
+        color: "#738F81",
+        letterSpacing: 0.5,
+        fontSize: 26,
+        textShadowColor: 'rgba(0, 0, 0, 0.25)', // Shadow color
+        textShadowOffset: { width: 4, height: 4 }, // Offset for the shadow
+        textShadowRadius: 10, // Radius of the shadow blur
+    },
+
+    frmEdit: {
+        width: '90%',
+        height: 50,
+        marginHorizontal: "auto",
+        borderColor: "#626262",
+        borderRadius: 50,
+        borderWidth: 0.6,
+        paddingStart: 20,
+        fontSize: 12,
+        fontWeight: "400",
+        color: "#626262",
+        letterSpacing: 3,
+    },
+
+    btnSave: {
+        width: 118,
+        height: 42,
+        borderRadius: 50,
+        backgroundColor: "#738F81",
+        marginHorizontal: "auto",
+        marginTop: 40,
+        alignItems: "center",
+    },
+
+    textInBtn: {
+        fontSize: 16,
+        fontWeight: "800",
+        color: "#FFFFFF",
+        letterSpacing: 3,
+        marginVertical: "auto"
+    }
+
 });
 
 export default ChangeEmailScreen;
